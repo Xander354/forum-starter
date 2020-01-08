40e07a9fdf05d7f41bc01b93406c4ccbcf7b8de9
Xander354 <36720391+Xander354@users.noreply.github.com>
2020-01-08 13:28:38 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def home():
+  return flask.render_template('index.html')
+
+if __name__ == "__main__":
+  app.run()
